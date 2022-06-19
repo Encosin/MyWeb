845cce9897e1da2e1d9725f45e4ffa99d2d03f2d
Byeonghun-Choi <cbh3612@naver.com>
2022-06-20 01:39:20 +0900

    .

---
+++ js/example.js
@@ -0,0 +1,30 @@
+document.write("책에 자바스크립트는 대소문자를 구분해야 합니다 라고 나와있다.");
+// 변수 선언 방법
+var A;
+var B = 10; 
+
+var box;
+box = 100;
+
+var s = "javascript";
+var num = "100";
+var tag = "<h1> String </h1>";
+
+
+{/* <script>
+    var num = 100;
+    var str = "자바스크립트";
+
+    document.write(typeof num, "<br>");  // number
+    document.write(typeof str);  // srting
+</script> */}
+
+<script>
+    var num1 = 15;
+    var num2 = 2;
+    var result;
+    result = num1 + num2;
+    document.write(result, "<br>"); // 17
+    result = num1 - num2;
+    document.write(result, "<br>"); // 13
+</script>
